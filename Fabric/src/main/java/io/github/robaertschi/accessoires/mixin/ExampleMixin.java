af49c654045db84b760d658216a7c5c144fc6e63
robin <robinbaertschi2@gmail.com>
2022-08-19 19:04:04 +0200

    Added Gradle Publish support and cleaned some code.

--- Fabric/src/main/java/io/github/robaertschi/accessoires/mixin/ExampleMixin.java
+++ Fabric/src/main/java/io/github/robaertschi/accessoires/mixin/ExampleMixin.java
@@ -13,9 +13,5 @@ public class ExampleMixin {
     
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        
-        Constants.LOG.info("This line is printed by an example mod mixin from Fabric!");
-        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-        Constants.LOG.info("Classloader: {}", this.getClass().getClassLoader());
     }
 }
